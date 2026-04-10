0ee26399e685b686be07c0166df5a9d8c6878157
Fiozi Amer Reza <fare79215@edu.ucl.dk>
2026-04-10 17:23:01 +0200

    Added Genre og Stand I model folder

---
+++ Genspil/Genspil/Models/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Genspil.Models
+{
+    public class Stand
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id},{Navn}";
+        }
+
+        public static Stand FromString(string data)
+        {
+            string[] parts = data.Split(',');
+
+            return new Stand
+            {
+                Id = int.Parse(parts[0]),
+                Navn = parts[1]
+            };
+        }
+    }
+}
---
+++ Genspil/Genspil/Models/Genre.cs
@@ -0,0 +1,24 @@
+﻿namespace Genspil.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id},{Navn}";
+        }
+
+        public static Genre FromString(string data)
+        {
+            string[] parts = data.Split(',');
+
+            return new Genre
+            {
+                Id = int.Parse(parts[0]),
+                Navn = parts[1]
+            };
+        }
+    }
+}
