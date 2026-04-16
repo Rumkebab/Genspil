1ccb2b6c479e268e01edde3d2175624c78f5b6c1
Fiozi Amer Reza <fare79215@edu.ucl.dk>
2026-04-16 17:03:48 +0200

    ændret filsti

--- Genspil/Genspil/Program.cs
+++ Genspil/Genspil/Program.cs
@@ -8,7 +8,7 @@ namespace Genspil
         public static void Main(string[] args)
         {
             // Sti til tekstfilen med spillene
-            string filsti = "Datafiler/spil.txt";
+            string filsti = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Datafiler\spil.txt");
 
             // Læser spillene ind og starter hovedmenuen
             List<Spil> spilListe = SpilDataHandler.LæsFraFil(filsti);
