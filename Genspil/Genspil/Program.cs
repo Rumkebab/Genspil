07e73edaacb62aa4a8f32483e943e194f63dcc20
Amma79223 <anders@andersmathiesen.dk>
2026-04-16 10:06:07 +0200

    retter datahandler filsti

--- Genspil/Genspil/Program.cs
+++ Genspil/Genspil/Program.cs
@@ -1,4 +1,5 @@
-﻿using Genspil.Klasser;
+﻿
+using Genspil.Klasser;
 using Genspil.Data;
 
 namespace Genspil
@@ -8,8 +9,8 @@ namespace Genspil
         public static void Main(string[] args)
         {
             // Sti til filen med spillene
-            string filsti = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Datafiler\spil.txt");
-            filsti = Path.GetFullPath(filsti);
+            string filsti = "Datafiler/spil.txt";
+            
 
             // Læser spillene fra filen ved opstart
             List<Spil> spilListe = SpilDataHandler.LæsFraFil(filsti);
